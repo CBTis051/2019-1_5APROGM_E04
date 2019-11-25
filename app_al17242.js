797f1c505d31d00a06d452a472226d3b6c7d8f1f
al17242 <55164926+al17242@users.noreply.github.com>
2019-11-25 11:07:39 -0700

    Update app_al17242.js

--- app_al17242.js
+++ app_al17242.js
@@ -2,7 +2,7 @@ var http = require('http');
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola mundo Node JS Repo GIT - Paulina Guadalupe Sánchez Galicia/n');
+    res.end('Hola mundo Node JS Repo GIT - Paulina Guadalupe Sánchez Galicia\n');
 }).listen(8081);
 console.log('Server running on port 8081');
 
