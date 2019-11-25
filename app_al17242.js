2e888f9b63fadaf67f5a04d5bc25f021dac4f300
Paulina Guadalupe Sánchez Galicia <al17242@cbtis051.edu.mx>
2019-11-25 15:16:13 -0700

    Commit inicial del server

--- app_al17242.js
+++ app_al17242.js
@@ -1,8 +1,8 @@
+
 var http = require('http');
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola mundo Node JS Repo GIT - Paulina Guadalupe Sánchez Galicia/n');
+    res.end('Hola mundo Node JS Repo GIT - Paulina Guadalupe Sánchez Galicia\n');
 }).listen(8081);
 console.log('Server running on port 8081');
-
