55c807cf649ad61e3df6fa513be8d7970b61a502
Paulina Guadalupe Sánchez Galicia <al17242@cbtis051.edu.mx>
2019-11-26 10:50:53 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17214.js
+++ app_al17214.js
@@ -1,7 +1,14 @@
 var http = require('http');
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    
+    var objeto = {
+        mensaje : 'hola mundo Node JS Repo GIT- Lizarraga Peinado Amahirani Michelle\n'
+    };
 
-    res.end('Hola mundo Node JS Repo GIT - Lizarraga Peinado Amahirani Michelle \n');
+    var json = JSON.stringify(objeto);
+
+    res.end(jason);
+
 }).listen(8081);
 console.log('Server running on port 8081');
