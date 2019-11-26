e9e0c2839cec94e246fa4bc9beac23717a47061e
Salma Yareth Ibarra Flores <al17209@cbtis051.edu.mx>
2019-11-26 10:48:12 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17209.js
+++ app_al17209.js
@@ -2,7 +2,11 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content_Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola mundo Node JS Repo GIT - Ibarra Flores Salma Yareth\n');
+    var objeto = {
+        mensaje : 'Hola mundo Node JS Repo GIT - Ibarra Flores Salma Yareth \n'
+    };
 
+    var json = JSON.stringify(objeto);
+    res.end(json);
 }).listen(8081);
 console.log('Server running on port 8081');
