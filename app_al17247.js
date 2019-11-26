f4616171fb4cbb5797f5b86688492b0c300cbac4
Adhara Vallejo Millán <al17247@cbtis051.edu.mx>
2019-11-26 11:11:32 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17247.js
+++ app_al17247.js
@@ -1,9 +1,16 @@
 
 var http = require('http');
 http.createServer(function (req, res) {
+
     res.writeHead(200, {'Content_Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola mundo Node JS Repo GIT - Vallejo Millán Adhara \n');
+    var objeto = {
+        mensaje : 'Hola mundo Node JS Repo GIT - Vallejo Millán Adhara \n'
+    };
+
+    var json = JSON.stringify(objeto);
+
+    res.end(json);
 
 }).listen(8081);
 console.log('Server running on port 8081');
